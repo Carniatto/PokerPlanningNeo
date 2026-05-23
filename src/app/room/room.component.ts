584ad2720d7f8f9b124d1cbfc405b373cf7b9cfd
Carniatto <mateus.carniatto@gmail.com>
2026-05-23 13:18:47 +0300

    feat: adjust column distribution to 30%/70% on WFHD screens

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -497,7 +497,7 @@ import { TaskListComponent } from '../components/task-list/task-list.component';
 
       .voting-and-tasks-container {
         display: grid;
-        grid-template-columns: minmax(0, 1.2fr) minmax(0, 0.8fr);
+        grid-template-columns: minmax(0, 3fr) minmax(0, 7fr);
         gap: 3rem;
         align-items: start;
       }
